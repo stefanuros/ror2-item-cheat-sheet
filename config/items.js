344b1be4fba9997fc85353a25d998a40da24db72
Stefan Urosevic <stefanuros@gmail.com>
2020-04-23 22:24:57 -0400

    Added items file schema and fixed some changed some eslint rules

---
+++ config/items.js
@@ -0,0 +1,14 @@
+module.exports = {
+  items: [
+    {
+      id: 0,
+      image: "url",
+      description: "",
+      name: "",
+      rarity: "common/legendary/lunar",
+      type: "active/passive",
+      statType: ["damage", "attackSpeed"],
+      wikiUrl: ""
+    }
+  ]
+};
