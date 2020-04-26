0ee9e965276cb90bb90d699ce3c3cfbcde284c61
Stefan Urosevic <stefanuros@gmail.com>
2020-04-26 01:13:28 -0400

    Adding all passive items to the list of items

--- config/constants.js
+++ config/constants.js
@@ -1,30 +1,59 @@
 const StackType = Object.freeze({
-  LINEAR: "linear",
-  HYPERBOLIC: "hyperbolic",
-  EXPONENTIAL: "exponential",
-  SPECIAL: "special",
+  LINEAR: Symbol("linear"),
+  HYPERBOLIC: Symbol("hyperbolic"),
+  EXPONENTIAL: Symbol("exponential"),
+  SPECIAL: Symbol("special"),
+  NONE: Symbol("none"),
 });
 
 const StatType = Object.freeze({
-  DAMAGE: "damage",
-  CHARGE: "charge",
+  DAMAGE: Symbol("Damage"),
+  CHARGE: Symbol("Charge"),
+  RADIUS: Symbol("Radius"),
+  SPRINT_SPEED: Symbol("Sprint Speed"),
+  DURATION: Symbol("Duration"),
+  HEAL: Symbol("Heal"),
+  MOVEMENT_SPEED: Symbol('Movement Speed'),
+  SHIELD: Symbol('Shield'),
+  CHANCE: Symbol("Chance"),
+  ATTACK_SPEED: Symbol("Attack Speed"),
+  COOLDOWN: Symbol("Cooldown"),
+  EXTRA_JUMP: Symbol("Extra Jump"),
+  TOTAL_HEALTH: Symbol("Total Health"),
+  THRESHOLD: Symbol("Threshold"),
+  ATTACK_SPEED_CAP: Symbol("Attack Speed Cap"),
+  ARMOUR: Symbol("Armour"),
+  BOOST: Symbol("Boost"),
+  LUCK: Symbol("Luck"),
+  ALLY_HEALTH: Symbol("Ally Health"),
+  ALLY_DAMAGE: Symbol("Ally Damage"),
+  CRITICAL_STRIKE_CHANCE: Symbol("Critical Strike Chance"),
+  HEALTH_REGENERATION: Symbol("Health Regeneration"),
+  AMOUNT: Symbol("Amount"),
+  GOLD: Symbol("Gold"),
+  HEAL_RATE: Symbol("Heal Rate"),
 });
 
 const ItemRarity = Object.freeze({
-  COMMON: "common",
-  UNCOMMON: "uncommon",
-  LEGENDARY: "legendary",
-  BOSS: "boss",
-  LUNAR: "lunar",
-  EQUIPMENT: "equipment",
-  LUNAR_EQUIPMENT: "lunarEquipment",
-  ELITE_EQUIPMENT: "eliteEquipment",
+  COMMON: Symbol("common"),
+  UNCOMMON: Symbol("uncommon"),
+  LEGENDARY: Symbol("legendary"),
+  BOSS: Symbol("boss"),
+  LUNAR: Symbol("lunar"),
+  EQUIPMENT: Symbol("equipment"),
+  LUNAR_EQUIPMENT: Symbol("lunarEquipment"),
+  ELITE_EQUIPMENT: Symbol("eliteEquipment"),
 });
 
 const Category = Object.freeze({
-  DAMAGE: "damage",
-  AI_BLACKLIST: "aiBlacklist",
-  UTILITY: "utility",
+  DAMAGE: Symbol("damage"),
+  AI_BLACKLIST: Symbol("aiBlacklist"),
+  UTILITY: Symbol("utility"),
+  HEALING: Symbol("healing"),
+  SPRINT_RELATED: Symbol("sprintRelated"),
+  ON_KILL_EFFECT: Symbol("onKillEffect"),
+  EQUIPMENT_RELATED: Symbol("equipmentRelated"),
+  WORLD_UNIQUE: Symbol("worldUnique"), // do we care about keeping this category?
 });
 
 module.exports = {
